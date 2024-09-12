72b1d2fadaad8a75b8f1cf2b3512d972ee223825
Abraham KONE <kone.abraham@jool-int.com>
2024-09-12 10:58:18 +0000

    Fin application

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { ImageBackground, Text, View } from "react-native";
+import { s } from "./App.style";
+import hotBackground from "./assets/hot.png";
+import coldBackground from "./assets/cold.png";
+
+import { ButtonConvert } from "./components/ButtonConvert/ButtonConvert";
+import { InputTemperature } from "./components/InputTemperature/InputTemperature";
+import { TemperatureDisplay } from "./components/TemperatureDisplay/TemperatureDisplay";
+import { DEFAULT_TEMPERATURE, DEFAULT_UNIT, UNITS } from "./constant";
+import {
+  getOppositUnit,
+  convertTemperatureTo,
+  isIceTemperature,
+} from "./services/temperature-service";
 
 export default function App() {
+  const [inputValue, setInputValue] = useState(DEFAULT_TEMPERATURE);
+  const [currentUnit, setCurrentUnit] = useState(DEFAULT_UNIT);
+  const [currentBackground, setCurrentBackground] = useState();
+  const oppositeUnit = getOppositUnit(currentUnit);
+
+  useEffect(() => {
+    const temperatureAsFloat = Number.parseFloat(inputValue);
+    if (!isNaN(temperatureAsFloat)) {
+      const isColdBackground = isIceTemperature(inputValue, currentUnit);
+      setCurrentBackground(isColdBackground ? coldBackground : hotBackground);
+    }
+  }, [inputValue, currentUnit]);
+
+  function getConvertedTemperature() {
+    const valueAsFloat = Number.parseFloat(inputValue);
+    return isNaN(valueAsFloat)
+      ? ""
+      : convertTemperatureTo(oppositeUnit, valueAsFloat).toFixed(1);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={currentBackground} style={s.container}>
+      <View style={s.workspace}>
+        <TemperatureDisplay
+          value={getConvertedTemperature()}
+          unit={oppositeUnit}
+        />
+        <InputTemperature
+          onChangeText={setInputValue}
+          defaultValue={DEFAULT_TEMPERATURE}
+          unit={currentUnit}
+        />
+        <View>
+          <ButtonConvert
+            onPress={() => {
+              setCurrentUnit(oppositeUnit);
+            }}
+            unit={currentUnit}
+          />
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
